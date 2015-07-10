fa76cd9eb7457f620339ee108450216e9b79c3be
Ricardo Mena C <rgacort12@gmail.com>
2015-07-10 13:36:41 -0600

    Change GenSens to Moteino

---
+++ Moteino/Moteino.cpp
@@ -0,0 +1,102 @@
+/*
+Easy handler for RFM69
+
+Ricardo Mena C
+ricardo@crcibernetica.com
+http://crcibernetica.com
+
+ License
+ **********************************************************************************
+ This program is free software; you can redistribute it 
+ and/or modify it under the terms of the GNU General    
+ Public License as published by the Free Software       
+ Foundation; either version 3 of the License, or        
+ (at your option) any later version.                    
+                                                        
+ This program is distributed in the hope that it will   
+ be useful, but WITHOUT ANY WARRANTY; without even the  
+ implied warranty of MERCHANTABILITY or FITNESS FOR A   
+ PARTICULAR PURPOSE. See the GNU General Public        
+ License for more details.                              
+                                                        
+ You should have received a copy of the GNU General    
+ Public License along with this program.
+ If not, see <http://www.gnu.org/licenses/>.
+                                                        
+ Licence can be viewed at                               
+ http://www.gnu.org/licenses/gpl-3.0.txt
+
+ Please maintain this license information along with authorship
+ and copyright notices in any redistribution of this code
+ **********************************************************************************
+*/
+
+#include "Moteino.h"
+
+Moteino::Moteino(uint8_t &node_Id, uint8_t frequency, const char *encryptK, uint8_t netw_id, boolean mote_type){
+	node_id = node_Id;
+	radio.initialize(frequency, node_id, netw_id);
+	if(mote_type){//is this a high power moteino?
+		radio.setHighPower();
+	}//end if
+	radio.encrypt(encryptK);//(const char*)
+}
+
+Moteino::~Moteino(){
+  //  delete(this);
+}
+
+boolean Moteino::moteino_receive(String &msg){
+  msg = "";//Delete old string
+  node_id_receive = radio.SENDERID;//Save node id
+  if (radio.receiveDone()){
+	//msg += radio.SENDERID;//is the same uint8_t than DEC
+	//msg += "<";//Close ID node identifier
+	for(uint8_t i = 0; i < radio.DATALEN; i++){
+	  msg += (char)radio.DATA[i];//This is the data
+	}//end for
+	//msg += ">";
+	if (radio.ACKRequested()){
+	  radio.sendACK();
+	  //msg += " - ACK sent";
+	}//end if
+	alert(3);
+	return true;
+  }//end if
+  return false;//
+}//end moteino_receive
+
+boolean Moteino:: moteino_send(uint8_t &gateway, const char *buffer, uint8_t bufferSize, uint8_t retries, uint8_t retryWaitTime){//retrines default 2, retriesWait default 40
+
+  if (radio.sendWithRetry(gateway, buffer, bufferSize, retries, retryWaitTime)){
+	alert(3);
+	return true;//ok!
+  }else{
+	return false;//nothing!
+  }//end if
+}//end moteino_send
+
+uint8_t Moteino::moteino_id_receive(){
+  return node_id_receive;
+}
+
+uint8_t Moteino::moteino_id(){
+  return node_id;
+}
+
+void Moteino::mote_sleep(){
+  Serial.flush();
+  radio.sleep();
+}//end mote_sleep
+
+
+void Moteino::alert(uint8_t t_delay){
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED,HIGH);
+  delay(t_delay);
+  digitalWrite(LED,LOW);
+}//end alert_incomming
+
+int Moteino::moteino_rssi(){
+  return radio.RSSI;
+}//end moteino_rssi
---
+++ Moteino/Moteino.h
@@ -0,0 +1,73 @@
+/*
+Easy handler for RFM69
+
+Ricardo Mena C
+ricardo@crcibernetica.com
+http://crcibernetica.com
+
+ License
+ **********************************************************************************
+ This program is free software; you can redistribute it 
+ and/or modify it under the terms of the GNU General    
+ Public License as published by the Free Software       
+ Foundation; either version 3 of the License, or        
+ (at your option) any later version.                    
+                                                        
+ This program is distributed in the hope that it will   
+ be useful, but WITHOUT ANY WARRANTY; without even the  
+ implied warranty of MERCHANTABILITY or FITNESS FOR A   
+ PARTICULAR PURPOSE. See the GNU General Public        
+ License for more details.                              
+                                                        
+ You should have received a copy of the GNU General    
+ Public License along with this program.
+ If not, see <http://www.gnu.org/licenses/>.
+                                                        
+ Licence can be viewed at                               
+ http://www.gnu.org/licenses/gpl-3.0.txt
+
+ Please maintain this license information along with authorship
+ and copyright notices in any redistribution of this code
+ **********************************************************************************
+*/
+
+#ifndef __MOTEINO_H__
+#define __MOTEINO_H__
+#include <Arduino.h>
+
+#define ONE_WIRE_BUS 7
+// Simplified Moteino Node Test
+
+#include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
+#include <SPI.h>
+
+#ifdef __AVR_ATmega1284P__
+  #define LED           15 // Moteino MEGAs have LEDs on D15
+  #define FLASH_SS      23 // and FLASH SS on D23
+#else
+  #define LED           9 // Moteinos have LEDs on D9
+  #define FLASH_SS      8 // and FLASH SS on D8
+#endif
+
+
+class Moteino{
+  private:
+    uint8_t node_id;//Default
+	uint8_t node_id_receive;
+	//const char *moteino_type;
+	//unsigned long uptime;
+	RFM69 radio;
+	void alert(uint8_t t_delay);
+  public:
+    Moteino(){};//Empty constructor
+	virtual ~Moteino();
+    Moteino(uint8_t &node_Id, uint8_t frequency, const char *encryptK, uint8_t netw_id, boolean mote_type=false);
+    //int moteino_init();//
+	boolean moteino_receive(String &msg);
+	boolean moteino_send(uint8_t &gateway, const char *buffer, uint8_t bufferSize, uint8_t retries=2, uint8_t retryWaitTime=40);// const void* buffer
+	void mote_sleep();
+   uint8_t moteino_id_receive();
+   uint8_t moteino_id();
+   int moteino_rssi();
+};
+#endif /* #ifndef __GENSENS_H__ */
